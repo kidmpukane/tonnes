9d2d7c9462d103ccc2cbed2a83ba6367eb8804a5
kidmpukane <lwandisozwane91@gmail.com>
2024-10-05 13:12:27 +0200

    complete pre links addition

--- tonnes/src/InfiniteScrollPage.jsx
+++ tonnes/src/InfiniteScrollPage.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 import HomePage from './pages/HomePage'
 import AboutPage from './pages/AboutPage'
 import { ObjectivesPage, ObjectivesTop } from './pages/ObjectivesPage'
+import myFingy from "./assets/finger.svg"
+import "./App.css"
 
 const InfiniteScrollPage = () => {
   return (
     <div>
         <HomePage/>
+         <img src={myFingy} alt="Coins" className="finger-svg"/>
         <ObjectivesPage/>
         <ObjectivesTop/>
         <AboutPage/>
